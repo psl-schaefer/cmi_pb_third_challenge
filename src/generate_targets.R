738b1a363f1d42d2fc1952d4c0fc1a06b17220b0
Philipp SL Schäfer <philipp.schaefer98@icloud.com>
2024-10-14 20:13:28 +0200

    adding feature selection script

--- src/generate_targets.R
+++ src/generate_targets.R
@@ -1,42 +1,40 @@
 
 acceptable_differences <- list(
   day_0 = list(
-    "d" = 0,
     "min_diff" = -7,
     "max_diff" = 0
   ),
   day_1 = list(
-    "d" = 0,
     "min_diff" = 1,
     "max_diff" = 2
   ),
   day_3 = list(
-    "d" = 0,
     "min_diff" = 3,
     "max_diff" = 6
   ),
   day_14 = list(
-    "d" = 0,
     "min_diff" = 12,
     "max_diff" = 16
+  ),
+  day_30 = list(
+    "min_diff" = 28,
+    "max_diff" = 40
   )
 )
 
 get_specimen_per_day <- function(meta_data) {
   specimen_list <- purrr::map(acceptable_differences, function(d_list) {
     meta_data %>%
-      dplyr::mutate(diff = actual_day_relative_to_boost - d_list$d) %>%
-      dplyr::filter(diff >= d_list$min_diff) %>%
-      dplyr::filter(diff <= d_list$max_diff) %>%
+      dplyr::filter(actual_day_relative_to_boost >= d_list$min_diff) %>%
+      dplyr::filter(actual_day_relative_to_boost <= d_list$max_diff) %>%
       dplyr::group_by(subject_id) %>%
-      dplyr::slice_min(abs(diff)) %>%
+      dplyr::slice_min(abs(actual_day_relative_to_boost)) %>%
       dplyr::ungroup() %>%
       dplyr::select(specimen_id, 
                     subject_id, 
                     dataset,
                     actual_day_relative_to_boost, 
-                    planned_day_relative_to_boost,
-                    diff)
+                    planned_day_relative_to_boost)
   })
   
   # check that no specimen is considered for two days?
@@ -49,6 +47,9 @@ get_specimen_per_day <- function(meta_data) {
   return(specimen_list)
 }
 
+# 1.1 Rank the individuals by IgG antibody levels against pertussis toxin (PT) that we detect in plasma 14 days post booster vaccinations.
+# 1.2 Rank the individuals by fold change of IgG antibody levels against pertussis toxin (PT) that we detect in plasma 14 days post booster vaccinations compared to titer values at day 0.
+# NOTE: Instead of fold change I will use logFC here
 generate_targets_task_1 <- function(meta_data, 
                                     experimental_data, 
                                     experimental_data_settings,
@@ -71,12 +72,16 @@ generate_targets_task_1 <- function(meta_data,
     tidyr::pivot_wider(names_from="day",
                        values_from=experimental_data_settings$plasma_ab_titer$value_col) %>%
     dplyr::mutate(fc = .data[[day_post_booster]] / .data[["day_0"]]) %>%
-    dplyr::mutate(baseline=day_0, task11=day_14, task12=fc) %>%
+    dplyr::mutate(log_fc = log(fc)) %>%
+    dplyr::mutate(baseline=day_0, task11=day_14, task12=log_fc) %>%
     tidyr::drop_na()
   
   return(targets_task_1)
 }
 
+# 2.1 Rank the individuals by predicted frequency of Monocytes on day 1 post boost after vaccination.
+# 2.2 Rank the individuals by fold change of predicted frequency of Monocytes on day 1 post booster vaccination compared to cell frequency values at day 0.
+# NOTE: Instead of fold change I will use logFC here
 generate_targets_task_2 <- function(meta_data, 
                                     experimental_data, 
                                     experimental_data_settings,
@@ -99,12 +104,17 @@ generate_targets_task_2 <- function(meta_data,
     tidyr::pivot_wider(names_from="day",
                        values_from=experimental_data_settings$pbmc_cell_frequency$value_col) %>%
     dplyr::mutate(fc = .data[[day_post_booster]] / .data[["day_0"]]) %>%
-    dplyr::mutate(baseline=day_0, task21=day_1, task22=fc) %>%
+    dplyr::mutate(log_fc = log(fc)) %>%
+    dplyr::mutate(baseline=day_0, task21=day_1, task22=log_fc) %>%
     tidyr::drop_na()
   
   return(targets_task_2)
 }
 
+# 3.1 Rank the individuals by predicted gene expression of CCL3 on day 3 post-booster vaccination.
+# 3.2 Rank the individuals by fold change of predicted gene expression of CCL3 on day 3 post booster vaccination compared to gene expression values at day 0.
+# NOTE: Instead of directly predicting expression, I will predict log expression
+# NOTE: Instead of fold change I will use logFC here
 generate_targets_task_3 <- function(meta_data, 
                                     experimental_data, 
                                     experimental_data_settings,
@@ -136,16 +146,65 @@ generate_targets_task_3 <- function(meta_data,
     tidyr::pivot_wider(names_from="day",
                        values_from=experimental_data_settings$pbmc_gene_expression$value_col) %>%
     dplyr::mutate(fc = .data[[day_post_booster]] / .data[["day_0"]]) %>%
-    dplyr::mutate(baseline=day_0, task31=day_3, task32=fc) %>%
+    dplyr::mutate(log_fc = log(fc)) %>%
+    dplyr::mutate(baseline=log(day_0), task31=log(day_3), task32=log_fc) %>%
     tidyr::drop_na()
   
   return(targets_task_3)
 }
 
+# 4.1 Rank the individuals based on their Th1/Th2 (IFN-γ/IL-5) polarization ratio on Day 30 post-booster vaccination.
+generate_targets_task_4 <- function(meta_data, 
+                                    experimental_data, 
+                                    experimental_data_settings,
+                                    specimen_list,
+                                    protein_meta) {
+  
+  day_post_booster = "day_30"
+  
+  # https://discuss.cmi-pb.org/t/announcement-bonus-prediction-task-for-the-3rd-public-cmi-pb-challenge/683
+  # ratio of IFNG / IL5
+  # NOTE: I assume here that "PT" stimulation is correct
+  # NOTE: Instead of the ratio I use the log ratio
+  targets <- experimental_data$t_cell_polarization %>%
+    dplyr::filter(!is.na(protein_id)) %>%
+    dplyr::left_join(protein_meta, by=c("protein_id"="uniprot_id")) %>%
+    dplyr::filter(stimulation=="PT", cytokine %in% c("IFNG", "IL5")) %>%
+    dplyr::select(specimen_id, cytokine, analyte_counts) %>%
+    tidyr::pivot_wider(names_from=cytokine, values_from=analyte_counts) %>%
+    dplyr::mutate(ratio = IFNG / IL5) %>%
+    dplyr::mutate(log_ratio = log(ratio))
+  
+  day_0 <- meta_data %>% 
+    dplyr::filter(specimen_id %in% specimen_list[["day_0"]][["specimen_id"]]) %>%
+    dplyr::mutate(day = "day_0") %>%
+    dplyr::select(specimen_id, subject_id, day) %>%
+    dplyr::left_join(targets, by="specimen_id") %>%
+    tidyr::drop_na() %>%
+    dplyr::select(subject_id, log_ratio, day)
+  
+  day_30 <- meta_data %>% 
+    dplyr::filter(specimen_id %in% specimen_list[[day_post_booster]][["specimen_id"]]) %>%
+    dplyr::mutate(day = day_post_booster) %>%
+    dplyr::select(specimen_id, subject_id, day) %>%
+    dplyr::left_join(targets, by="specimen_id") %>%
+    tidyr::drop_na() %>%
+    dplyr::select(subject_id, log_ratio, day)
+  
+  targets_task_4 <- dplyr::bind_rows(day_0, day_30) %>%
+    tidyr::pivot_wider(names_from=day, values_from=log_ratio) %>%
+    dplyr::mutate(baseline=day_0, task41=day_30) %>%
+    dplyr::filter(!is.na(task41)) %>%
+    dplyr::filter(!is.na(baseline)) # there are 3 subjects with missing baseline, which I also remove...
+  
+  return(targets_task_4)
+}
+
 generate_all_targets <- function(meta_data, 
                                  experimental_data, 
                                  experimental_data_settings,
-                                 gene_meta) {
+                                 gene_meta,
+                                 protein_meta) {
   
   specimen_list <- get_specimen_per_day(meta_data=meta_data)
   
@@ -193,13 +252,22 @@ generate_all_targets <- function(meta_data,
     dplyr::rename(target=task32) %>%
     dplyr::select(subject_id, baseline, target)
     
+  task_41 <- generate_targets_task_4(meta_data=meta_data, 
+                                     experimental_data=experimental_data, 
+                                     experimental_data_settings=experimental_data_settings,
+                                     specimen_list=specimen_list,
+                                     protein_meta=protein_meta) %>%
+    dplyr::rename(target=task41) %>%
+    dplyr::select(subject_id, baseline, target)
+  
   return(list(
     task_11 = task_11,
     task_12 = task_12,
     task_21 = task_21,
     task_22 = task_22,
     task_31 = task_31,
-    task_32 = task_32
+    task_32 = task_32,
+    task_41 = task_41
   ))
 }
 
--- src/normalize_integrate.R
+++ src/normalize_integrate.R
@@ -4,20 +4,20 @@ library(sva)
 
 
 normalize_experimental_data <- function(meta_data, 
-                                        experimental_data, 
+                                        raw_experimental_data, 
                                         gene_meta) {
   specimen_per_day <- get_specimen_per_day(meta_data=meta_data)
   normalized_data <- list()
   
   # 1) pbmc_cell_frequency: Median Baseline Normalization
-  baseline_medians <- experimental_data$pbmc_cell_frequency %>%
+  baseline_medians <- raw_experimental_data$pbmc_cell_frequency %>%
     dplyr::inner_join(specimen_per_day$day_0, by="specimen_id") %>%
     dplyr::group_by(dataset, cell_type_name) %>%
     dplyr::summarise(median = median(percent_live_cell, na.rm=TRUE), 
                      .groups="drop") %>%
     dplyr::mutate(median = ifelse(dplyr::near(median, 0), 1, median)) # prevent division by 0?
   
-  normalized_data$pbmc_cell_frequency <- experimental_data$pbmc_cell_frequency %>%
+  normalized_data$pbmc_cell_frequency <- raw_experimental_data$pbmc_cell_frequency %>%
     dplyr::left_join((meta_data %>% dplyr::select(specimen_id, dataset)),
                      by="specimen_id") %>%
     dplyr::left_join(baseline_medians, by=c("dataset", "cell_type_name")) %>%
@@ -28,7 +28,7 @@ normalize_experimental_data <- function(meta_data,
   # a) filter for meaningful genes
   feature_col <- experimental_data_settings$pbmc_gene_expression$feature_col
   value_col <- experimental_data_settings$pbmc_gene_expression$value_col
-  pbmc_gene_expression_wide <- experimental_data$pbmc_gene_expression %>%
+  pbmc_gene_expression_wide <- raw_experimental_data$pbmc_gene_expression %>%
     dplyr::select(dplyr::all_of(c("specimen_id", feature_col, value_col))) %>%
     tidyr::pivot_wider(names_from=dplyr::all_of(feature_col), 
                        values_from=dplyr::all_of(value_col)) %>%
@@ -83,7 +83,7 @@ normalize_experimental_data <- function(meta_data,
                         names_to=feature_col,
                         values_to=value_col)
   
-  normalized_data$pbmc_gene_expression_raw <- pbmc_gene_expression_wide %>%
+  normalized_data$pbmc_gene_expression_counts <- pbmc_gene_expression_wide %>%
     as.data.frame() %>%
     tibble::rownames_to_column(var="specimen_id") %>%
     tidyr::pivot_longer(cols=!c(specimen_id), 
@@ -92,13 +92,13 @@ normalize_experimental_data <- function(meta_data,
     
   
   # 3) plasma_ab_titer: Median Baseline Normalization
-  baseline_medians <- experimental_data$plasma_ab_titer %>%
+  baseline_medians <- raw_experimental_data$plasma_ab_titer %>%
     dplyr::inner_join(specimen_per_day$day_0, by="specimen_id") %>%
     dplyr::group_by(dataset, isotype_antigen) %>%
     dplyr::summarise(median = median(MFI, na.rm=TRUE), .groups="drop") %>%
     dplyr::mutate(median = ifelse(dplyr::near(median, 0), 1, median)) # prevent division by 0?
   
-  normalized_data$plasma_ab_titer <- experimental_data$plasma_ab_titer %>%
+  normalized_data$plasma_ab_titer <- raw_experimental_data$plasma_ab_titer %>%
     dplyr::left_join((meta_data %>% dplyr::select(specimen_id, dataset)),
                      by="specimen_id") %>%
     dplyr::left_join(baseline_medians, by=c("dataset", "isotype_antigen")) %>%
@@ -106,7 +106,7 @@ normalize_experimental_data <- function(meta_data,
     dplyr::select(-c(dataset, median))
   
   # 4) plasma_cytokine_concentration_by_legendplex: Median Baseline Normalization
-  baseline_medians <- experimental_data$plasma_cytokine_concentration_by_legendplex %>%
+  baseline_medians <- raw_experimental_data$plasma_cytokine_concentration_by_legendplex %>%
     dplyr::inner_join(specimen_per_day$day_0, by="specimen_id") %>%
     dplyr::group_by(dataset, protein_id) %>%
     dplyr::summarise(median = median(concentration, na.rm=TRUE), 
@@ -114,7 +114,7 @@ normalize_experimental_data <- function(meta_data,
     dplyr::mutate(median = ifelse(dplyr::near(median, 0), 1, median)) # prevent division by 0?
   
   normalized_data$plasma_cytokine_concentration_by_legendplex <- 
-    experimental_data$plasma_cytokine_concentration_by_legendplex %>%
+    raw_experimental_data$plasma_cytokine_concentration_by_legendplex %>%
     dplyr::left_join((meta_data %>% dplyr::select(specimen_id, dataset)),
                      by="specimen_id") %>%
     dplyr::left_join(baseline_medians, by=c("dataset", "protein_id")) %>%
@@ -124,35 +124,35 @@ normalize_experimental_data <- function(meta_data,
   # 5) plasma_cytokine_concentration_by_olink: No Normalization needed? 
   # TODO: Take into consideration that the CMI-PB consortium suggests median baseline normalization
   normalized_data$plasma_cytokine_concentration_by_olink <- 
-    experimental_data$plasma_cytokine_concentration_by_olink
+    raw_experimental_data$plasma_cytokine_concentration_by_olink
   
   # 6) t_cell_activation: No Normalization needed? (suggested by CMI-PB consortium)
   normalized_data$t_cell_activation <- 
-    experimental_data$t_cell_activation
+    raw_experimental_data$t_cell_activation
   
   # 7) t_cell_polarization: No Normalization needed? (suggested by CMI-PB consortium)
   normalized_data$t_cell_polarization <- 
-    experimental_data$t_cell_polarization
+    raw_experimental_data$t_cell_polarization
   
   # check that we did not forget any assay
-  stopifnot(dplyr::setequal(names(experimental_data), 
-                            names(normalized_data)[!names(normalized_data) %in% c("pbmc_gene_expression_raw")]))
+  stopifnot(dplyr::setequal(names(raw_experimental_data), 
+                            names(normalized_data)[!names(normalized_data) %in% c("pbmc_gene_expression_counts")]))
   
   return(normalized_data)
 }
 
 
-integrate_experimental_data <- function(meta_data, experimental_data) {
+integrate_experimental_data <- function(meta_data, normalized_experimental_data) {
   integrated_data <- list()
   
   # 1) pbmc_cell_frequency: Nothing
   integrated_data$pbmc_cell_frequency <-
-    experimental_data$pbmc_cell_frequency
+    normalized_experimental_data$pbmc_cell_frequency
   
   # 2) pbmc_gene_expression: ComBat-seq
   feature_col <- experimental_data_settings$pbmc_gene_expression$feature_col
   value_col <- experimental_data_settings$pbmc_gene_expression$value_col
-  pbmc_gene_expression_wide <- experimental_data$pbmc_gene_expression %>%
+  pbmc_gene_expression_wide <- normalized_experimental_data$pbmc_gene_expression_counts %>%
     dplyr::select(dplyr::all_of(c("specimen_id", feature_col, value_col))) %>%
     tidyr::pivot_wider(names_from=dplyr::all_of(feature_col), 
                        values_from=dplyr::all_of(value_col)) %>%
@@ -171,6 +171,7 @@ integrate_experimental_data <- function(meta_data, experimental_data) {
                                            ncol = ncol(pbmc_gene_expression_corrected), 
                                            dimnames = dimnames(pbmc_gene_expression_corrected))
   
+  # just to make sure, if any specimen have been removed by combat-seq
   pbmc_gene_expression_meta <- tibble::column_to_rownames(meta_data, var="specimen_id") %>%
     .[rownames(pbmc_gene_expression_corrected), ] 
   
@@ -203,7 +204,7 @@ integrate_experimental_data <- function(meta_data, experimental_data) {
                         names_to=feature_col,
                         values_to=value_col)
   
-  integrated_data$pbmc_gene_expression_only_corrected <- 
+  integrated_data$pbmc_gene_expression_counts <- 
     pbmc_gene_expression_corrected %>%
     as.data.frame() %>%
     tibble::rownames_to_column(var="specimen_id") %>%
@@ -214,7 +215,7 @@ integrate_experimental_data <- function(meta_data, experimental_data) {
   # 3) plasma_ab_titer: 
   # TODO: Note sure yet
   integrated_data$plasma_ab_titer <- 
-    experimental_data$plasma_ab_titer
+    normalized_experimental_data$plasma_ab_titer
   
   # 4) plasma_cytokine_concentration_by_legendplex: 
   #integrated_data$plasma_cytokine_concentration_by_legendplex <- 
@@ -223,7 +224,7 @@ integrate_experimental_data <- function(meta_data, experimental_data) {
   value_col <- experimental_data_settings$plasma_cytokine_concentration_by_legendplex$value_col
 
   plasma_cytokine_concentration_by_legendplex_wide <- 
-    experimental_data$plasma_cytokine_concentration_by_legendplex %>%
+    normalized_experimental_data$plasma_cytokine_concentration_by_legendplex %>%
     dplyr::select(dplyr::all_of(c("specimen_id", feature_col, value_col))) %>%
     tidyr::pivot_wider(names_from=dplyr::all_of(feature_col), 
                        values_from=dplyr::all_of(value_col)) %>%
@@ -253,15 +254,18 @@ integrate_experimental_data <- function(meta_data, experimental_data) {
   # 5) plasma_cytokine_concentration_by_olink: No Normalization needed? 
   # TODO: Take into consideration that the CMI-PB consortium suggests median baseline normalization
   integrated_data$plasma_cytokine_concentration_by_olink <- 
-    experimental_data$plasma_cytokine_concentration_by_olink
+    normalized_experimental_data$plasma_cytokine_concentration_by_olink
   
   # 6) t_cell_activation: No Normalization needed? (suggested by CMI-PB consortium)
   integrated_data$t_cell_activation <- 
-    experimental_data$t_cell_activation
+    normalized_experimental_data$t_cell_activation
   
   # 7) t_cell_polarization: No Normalization needed? (suggested by CMI-PB consortium)
   integrated_data$t_cell_polarization <- 
-    experimental_data$t_cell_polarization
+    normalized_experimental_data$t_cell_polarization
+  
+  # check that we did not forget any assay
+  stopifnot(dplyr::setequal(names(normalized_experimental_data), names(integrated_data)))
   
   return(integrated_data)  
 }
